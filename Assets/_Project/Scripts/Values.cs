14d183f242580877ab939c0f001dfcb8c0fbc6e9
Bushra Ashraf <bushra.ashraf284@gmail.com>
2023-08-03 17:40:14 +0500

    Added Dynamic Text and Translation
    -> Getting Dynamic Date
    -> Getting Dynamic Values
    -> Different Translation Setting

---
+++ Assets/_Project/Scripts/Values.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Localization.Components;
+
+public class Values : MonoBehaviour
+{
+    public string Character = "Blaze";
+    public int StolenAmount = 100;
+    public DateTime Today = DateTime.Now;
+
+    
+}
